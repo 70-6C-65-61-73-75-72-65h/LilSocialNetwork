b53c470dc867f54e3f81d3d01c8f7183612f9ade
Max Ulshin <max.ulshin.max@istat.com.ua>
2020-09-22 04:19:00 +0300

    Updates

---
+++ precache-manifest.5f73e16440bf3e7b1a53c2f4041475f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26ee2a21c0ec6520e00077686f79f83",
+    "url": "/LilSocialNetwork/index.html"
+  },
+  {
+    "revision": "140ed7cee23b2067b031",
+    "url": "/LilSocialNetwork/static/css/main.4e3a0e3b.chunk.css"
+  },
+  {
+    "revision": "cc04ba96c1c327a39c77",
+    "url": "/LilSocialNetwork/static/js/2.a55a6d06.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/LilSocialNetwork/static/js/2.a55a6d06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "140ed7cee23b2067b031",
+    "url": "/LilSocialNetwork/static/js/main.e1e023c9.chunk.js"
+  },
+  {
+    "revision": "880968ca5545e3630f57",
+    "url": "/LilSocialNetwork/static/js/runtime-main.494f4788.js"
+  },
+  {
+    "revision": "d5c02aa6c461a18d08978be560c78740",
+    "url": "/LilSocialNetwork/static/media/preloader1.d5c02aa6.gif"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/LilSocialNetwork/static/media/user.8b1dbb1f.png"
+  }
+]);
