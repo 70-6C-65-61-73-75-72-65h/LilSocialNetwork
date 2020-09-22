9434c3124752286f55e86a30974586c4b4abd06e
Max Ulshin <max.ulshin.max@istat.com.ua>
2020-09-22 18:14:40 +0300

    added basename to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import {BrowserRouter} from "react-router-dom";
-// import {posts, messages, dialogs} from './redux/state'; для не дефолтного експорта надо куча приемников и расширения файла а  не путь к модулю
-// import state from './redux/state';
+import {BrowserRouter} from "react-router-dom"; 
 import store from './redux/redux-store';
-import { Provider } from 'react-redux';
-// import serverUsers from './serverApi/serverUsers';
+import { Provider } from 'react-redux'; 
 
 import reactDebugHooks from 'react-debug-hooks'
 reactDebugHooks(React)
@@ -17,13 +14,10 @@ reactDebugHooks(React)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-
-// let rerenderEntireTree = (state) => {
+ 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-
-    {/* <Route exact path='https://localhost:3000/api/1.0/users' render={ () => <serverUsers />}/> */}
+    <BrowserRouter  basename={process.env.PUBLIC_URL}> 
 
     <Provider store={store} >
       <App/>
@@ -31,18 +25,6 @@ ReactDOM.render(
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
-  );
-// }
-
-// rerenderEntireTree(store.getState());
-// // store.subscribe(rerenderEntireTree);
-
-// store.subscribe(() => {
-//   let state = store.getState();
-//   rerenderEntireTree(state);
-// });
-
-
-// renderEntireTree(state, addPost, updateNewPostText);
+  ); 
 
 serviceWorker.unregister();
